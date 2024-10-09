49d5398f844c3829e63127e175c770709833aa71
unknown <rayhan.senbag.bd@gmail.com>
2024-10-09 19:35:59 +0600

    dsf

--- serverServices/firebase-server/firebase_server_init.ts
+++ serverServices/firebase-server/firebase_server_init.ts
@@ -1,12 +1,14 @@
 import firebase from "firebase-admin";
 
 export function firebase_server_init() {
-  if (firebase.app.length) return;
-  return firebase.initializeApp({
-    credential: firebase.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    }),
-  });
+  return firebase.initializeApp(
+    {
+      credential: firebase.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      }),
+    },
+    "admin"
+  );
 }
